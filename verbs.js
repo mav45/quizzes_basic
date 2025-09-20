ff233aa7ab9cc5684cbbfecfed41f69145db8031
mav45 <62916125+mav45@users.noreply.github.com>
2025-09-20 15:09:19 -0400

    Update verbs.js

--- verbs.js
+++ verbs.js
@@ -1,21 +1,37 @@
 const verbs = [
-  // Present
+  // HABLAR (to speak)
+
+  // Indicative - Simple Tenses
   { infinitive:"hablar", translation:"to speak", tense:"present",
-    conjugations:{yo:"hablo", tú:"hablas", "él/ella/usted":"habla", nosotros:"hablamos", vosotros:"habláis", "ellos/ellas/ustedes":"hablan"} },
-  { infinitive:"comer", translation:"to eat", tense:"present",
-    conjugations:{yo:"como", tú:"comes", "él/ella/usted":"come", nosotros:"comemos", vosotros:"coméis", "ellos/ellas/ustedes":"comen"} },
-  { infinitive:"vivir", translation:"to live", tense:"present",
-    conjugations:{yo:"vivo", tú:"vives", "él/ella/usted":"vive", nosotros:"vivimos", vosotros:"vivís", "ellos/ellas/ustedes":"viven"} },
-  
-  // Preterite
+    conjugations:{yo:"hablo", tú:"hablas", "él/ella/usted":"habla", nosotros:"hablamos", vosotros:"habláis", "ellos/ellas/ustedes":"hablan"}},
   { infinitive:"hablar", translation:"to speak", tense:"preterite",
-    conjugations:{yo:"hablé", tú:"hablaste", "él/ella/usted":"habló", nosotros:"hablamos", vosotros:"hablasteis", "ellos/ellas/ustedes":"hablaron"} },
-  { infinitive:"comer", translation:"to eat", tense:"preterite",
-    conjugations:{yo:"comí", tú:"comiste", "él/ella/usted":"comió", nosotros:"comimos", vosotros:"comisteis", "ellos/ellas/ustedes":"comieron"} },
-  
-  // Imperfect
-  { infinitive:"vivir", translation:"to live", tense:"imperfect",
-    conjugations:{yo:"vivía", tú:"vivías", "él/ella/usted":"vivía", nosotros:"vivíamos", vosotros:"vivíais", "ellos/ellas/ustedes":"vivían"} },
-  { infinitive:"ser", translation:"to be", tense:"imperfect",
-    conjugations:{yo:"era", tú:"eras", "él/ella/usted":"era", nosotros:"éramos", vosotros:"erais", "ellos/ellas/ustedes":"eran"} }
+    conjugations:{yo:"hablé", tú:"hablaste", "él/ella/usted":"habló", nosotros:"hablamos", vosotros:"hablasteis", "ellos/ellas/ustedes":"hablaron"}},
+  { infinitive:"hablar", translation:"to speak", tense:"imperfect",
+    conjugations:{yo:"hablaba", tú:"hablabas", "él/ella/usted":"hablaba", nosotros:"hablábamos", vosotros:"hablabais", "ellos/ellas/ustedes":"hablaban"}},
+  { infinitive:"hablar", translation:"to speak", tense:"future",
+    conjugations:{yo:"hablaré", tú:"hablarás", "él/ella/usted":"hablará", nosotros:"hablaremos", vosotros:"hablaréis", "ellos/ellas/ustedes":"hablarán"}},
+  { infinitive:"hablar", translation:"to speak", tense:"conditional",
+    conjugations:{yo:"hablaría", tú:"hablarías", "él/ella/usted":"hablaría", nosotros:"hablaríamos", vosotros:"hablaríais", "ellos/ellas/ustedes":"hablarían"}},
+
+  // Indicative - Perfect Tenses
+  { infinitive:"hablar", translation:"to speak", tense:"present perfect",
+    conjugations:{yo:"he hablado", tú:"has hablado", "él/ella/usted":"ha hablado", nosotros:"hemos hablado", vosotros:"habéis hablado", "ellos/ellas/ustedes":"han hablado"}},
+  { infinitive:"hablar", translation:"to speak", tense:"pluscuamperfecto",
+    conjugations:{yo:"había hablado", tú:"habías hablado", "él/ella/usted":"había hablado", nosotros:"habíamos hablado", vosotros:"habíais hablado", "ellos/ellas/ustedes":"habían hablado"}},
+  { infinitive:"hablar", translation:"to speak", tense:"future perfect",
+    conjugations:{yo:"habré hablado", tú:"habrás hablado", "él/ella/usted":"habrá hablado", nosotros:"habremos hablado", vosotros:"habréis hablado", "ellos/ellas/ustedes":"habrán hablado"}},
+  { infinitive:"hablar", translation:"to speak", tense:"conditional perfect",
+    conjugations:{yo:"habría hablado", tú:"habrías hablado", "él/ella/usted":"habría hablado", nosotros:"habríamos hablado", vosotros:"habríais hablado", "ellos/ellas/ustedes":"habrían hablado"}},
+
+  // Subjunctive - Simple
+  { infinitive:"hablar", translation:"to speak", tense:"present subjunctive",
+    conjugations:{yo:"hable", tú:"hables", "él/ella/usted":"hable", nosotros:"hablemos", vosotros:"habléis", "ellos/ellas/ustedes":"hablen"}},
+  { infinitive:"hablar", translation:"to speak", tense:"imperfect subjunctive",
+    conjugations:{yo:"hablara", tú:"hablaras", "él/ella/usted":"hablara", nosotros:"habláramos", vosotros:"hablarais", "ellos/ellas/ustedes":"hablaran"}},
+
+  // Subjunctive - Perfect
+  { infinitive:"hablar", translation:"to speak", tense:"present perfect subjunctive",
+    conjugations:{yo:"haya hablado", tú:"hayas hablado", "él/ella/usted":"haya hablado", nosotros:"hayamos hablado", vosotros:"hayáis hablado", "ellos/ellas/ustedes":"hayan hablado"}},
+  { infinitive:"hablar", translation:"to speak", tense:"past perfect subjunctive",
+    conjugations:{yo:"hubiera hablado", tú:"hubieras hablado", "él/ella/usted":"hubiera hablado", nosotros:"hubiéramos hablado", vosotros:"hubierais hablado", "ellos/ellas/ustedes":"hubieran hablado"}}
 ];
